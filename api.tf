fed4cdae6304301b40ab2314f5c45afff13cac7b
Krzysztof Stasiak <krzsta@proton.me>
2026-06-22 15:47:24 +0200

    updated: added *.zip to gitignore and remove project plan from repo

--- api.tf
+++ api.tf
@@ -14,3 +14,23 @@ resource "aws_api_gateway_method" "GetStatusMethod" {
     http_method = "GET"
     authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "GetStatusIntegration" {
+    rest_api_id = aws_api_gateway_rest_api.WorkoutStatsAPI.id
+    resource_id = aws_api_gateway_resource.StatusResource.id
+    http_method = aws_api_gateway_method.GetStatusMethod.http_method
+    integration_http_method = "POST"
+    type = "AWS_PROXY"
+    uri = aws_lambda_function.status_lambda.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "WorkoutStatsAPIDeployment" {
+    depends_on = [aws_api_gateway_integration.GetStatusIntegration]
+    rest_api_id = aws_api_gateway_rest_api.WorkoutStatsAPI.id
+}
+
+resource "aws_api_gateway_stage" "WorkoutStatsAPIStage" {
+    stage_name = "prod"
+    rest_api_id = aws_api_gateway_rest_api.WorkoutStatsAPI.id
+    deployment_id = aws_api_gateway_deployment.WorkoutStatsAPIDeployment.id
+}
