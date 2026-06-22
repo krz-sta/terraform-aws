8288b9a53b083a729c828bbc517ce05222371772
Krzysztof Stasiak <krzsta@proton.me>
2026-06-22 14:45:17 +0200

    add: ddb for session history

--- storage.tf
+++ storage.tf
@@ -18,3 +18,21 @@ resource "aws_dynamodb_table" "DBActiveSessions" {
         type = "S"
     }
 }
+
+resource "aws_dynamodb_table" "DBSessionHistory" {
+    name = "DBSessionHistory"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "UserId"
+    range_key = "SessionId"
+
+    attribute {
+        name = "UserId"
+        type = "S"
+    }
+
+    attribute {
+        name = "SessionId"
+        type = "S"
+    }
+}
+
