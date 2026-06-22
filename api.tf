6957fab313c103000c910afc8244c01be64e2fdf
Krzysztof Stasiak <krzsta@proton.me>
2026-06-22 14:51:06 +0200

    /status resource and method for api status check

--- api.tf
+++ api.tf
@@ -1,3 +1,16 @@
 resource "aws_api_gateway_rest_api" "WorkoutStatsAPI" {
     name = "WorkoutStatsAPI"
 }
+
+resource "aws_api_gateway_resource" "StatusResource" {
+    rest_api_id = aws_api_gateway_rest_api.WorkoutStatsAPI.id
+    parent_id = aws_api_gateway_rest_api.WorkoutStatsAPI.root_resource_id
+    path_part = "status"
+}
+
+resource "aws_api_gateway_method" "GetStatusMethod" {
+    rest_api_id = aws_api_gateway_rest_api.WorkoutStatsAPI.id
+    resource_id = aws_api_gateway_resource.StatusResource.id
+    http_method = "GET"
+    authorization = "NONE"
+}
