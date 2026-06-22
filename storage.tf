8f9cbda8ae763021e4e3481f53830bba291076e8
Krzysztof Stasiak <krzsta@proton.me>
2026-06-22 14:43:56 +0200

    add: ddb for active sessions

--- storage.tf
+++ storage.tf
@@ -1,3 +1,20 @@
 resource "aws_s3_bucket" "S3WorkoutsArchive" {
     bucket_prefix = "s3-workouts-archive"
 }
+
+resource "aws_dynamodb_table" "DBActiveSessions" {
+    name = "DBActiveSessions"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "UserId"
+    range_key = "SessionId"
+
+    attribute {
+        name = "UserId"
+        type = "S"
+    }
+
+    attribute {
+        name = "SessionId"
+        type = "S"
+    }
+}
